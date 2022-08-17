21b18f92896fdcdaa8d72c9f4f94b3bf3487fa41
Vladislav Kvaskov <byakovich98@mail.ru>
2022-08-17 20:36:22 +0300

    end of the table

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -3,24 +3,46 @@ import api from "../api/index.js"
 
 const Users = () => {
 	const [users, setUsers] = useState(api.users.fetchAll());
+
+
 	const handleDelete = (userId) => {
-
+		setUsers((prevState) => prevState.filter(user => user._id !== userId))
 	};
+
+
 	const renderPhrase = (number) => {
-
+		let stringUsers = '';
+		switch(true) {
+			case number > 4 && number <= 14:
+				stringUsers = 'человек тусанут';
+				break;
+			case number > 1 && number < 5:
+				stringUsers = 'человека тусанут';
+				break;
+			case number === 1:
+				stringUsers = 'человек тусанет';
+				break;
+		}
+		return stringUsers;
 	};
 
 
+	const BustingQualities = (array) => {
+		return array.map((qualities) => {
+			return <span className={'badge m-1 text-bg-' + qualities.color} key={qualities._id}>{qualities.name}</span>
+		})
+	}
 
-	const QuantityUsers = (array) => {
+
+	let QuantityUsers = (array) => {
 		return array.map((user) => (
-			<tr>
-				<td key={user._id}>{user.name}</td>
-				<td>{}</td>
+			<tr key = {user._id}>
+				<td>{user.name}</td>
+				<td>{BustingQualities(user.qualities)}</td>
 				<td>{user.profession.name}</td>
 				<td>{user.completedMeetings}</td>
 				<td>{user.rate + '/5'}</td>
-				<td><button className='btn text-bg-danger'>delete</button></td>
+				<td><button className='btn text-bg-danger' onClick={()=>handleDelete(user._id)}>delete</button></td>
 			</tr>
 		))
 	};
@@ -29,21 +51,28 @@ const Users = () => {
 
 	return (
 		<>
-			<table className="table">
-			  <thead>
-			    <tr>
-			      <th scope="col">Имя</th>
-			      <th scope="col">Качества</th>
-			      <th scope="col">Профессия</th>
-			      <th scope="col">Встретился, раз</th>
-			      <th scope="col">Оценка</th>
-			      <th scope="col"></th>
-			    </tr>
-			  </thead>
-			  <tbody>
-			  	{QuantityUsers(users)}
-			  </tbody>
-			</table>
+			<h2 className={'badge ' + (users.length > 0 ? 'bg-primary' : 'bg-danger')}>
+				{users.length > 0 
+					? users.length + ' ' + renderPhrase(users.length) + ' с тобой сегодня'
+					: 'Никто с тобой не тусанет'}
+			</h2>
+			{users.length > 0 &&
+				<table className="table">
+				  <thead>
+				    <tr>
+				      <th scope="col">Имя</th>
+				      <th scope="col">Качества</th>
+				      <th scope="col">Профессия</th>
+				      <th scope="col">Встретился, раз</th>
+				      <th scope="col">Оценка</th>
+				      <th scope="col"></th>
+				    </tr>
+				  </thead>
+				  <tbody>
+				  	{QuantityUsers(users)}
+				  </tbody>
+				</table>
+			}
 		</>
 	);
 };
